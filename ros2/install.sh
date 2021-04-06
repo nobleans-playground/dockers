fe0eeb7438de736e4a60fc0cf820282e9b0e5daa
Yanick Douven <yanick.douven@nobleo.nl>
2021-04-06 18:00:20 +0200

    Improved install script

--- ros2/install.sh
+++ ros2/install.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-sudo apt-get install docker-ce nvidia-docker2 python3-pip zenity
+curl https://get.docker.com | sh
+sudo systemctl --now enable docker
+
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+
+sudo apt-get update
+sudo apt-get install -y nvidia-docker2
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+
+sudo systemctl restart docker
+
+sudo apt-get install python3-pip zenity
 sudo python3 -m pip install docker-compose
 
 ws_loc=$(zenity --file-selection --directory --title="Please select a folder for your workspace")
